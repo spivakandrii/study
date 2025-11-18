f4aae6c849962a5399a90fe5532054c9d388b8fa
Andrii Spivak <aspi@diceus.com>
2025-11-18 09:39:05 +0200

    Loop. Stage 1.

---
+++ Loop/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Loop while!");
+
+var count = 0;
+
+while (count < 1000)
+{
+    count += 10;
+    Console.WriteLine("Infinite loop!");
+}
+
+//Console.WriteLine("Loop do while!");
+
+//var count = 0;
+
+//do
+//{
+//    //count += 10;
+
+//    count = count + 10;
+
+//    Console.WriteLine($"Number: {count}");
+//} while (count < 1);
+
+
+
+//Console.WriteLine("Loop for!");
+
+//for (int i = 0; ;)
+//{
+//    Console.WriteLine($"Number: {i}");
+
+//    if (i == 1000)
+//    {
+//        Console.WriteLine("Reached 100, breaking the loop.");
+//        break;
+//    }
+
+//    i += 10;
+//}
+
