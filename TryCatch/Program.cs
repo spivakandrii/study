f6e3da8815d643212e381520045143780a73c4c3
Andrii Spivak <aspi@diceus.com>
2025-11-25 08:53:52 +0200

    try catch finally

---
+++ TryCatch/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Please enter number!");
+
+var numberString = Console.ReadLine();
+
+int number = 0;
+
+try
+{
+    number = ParseNumber(numberString);
+}
+catch
+{
+    Console.WriteLine("Our first error. Please enter Again");
+
+    numberString = Console.ReadLine();
+
+    number = ParseNumber(numberString);
+}
+finally
+{
+    Console.WriteLine($"Our number is {number}");
+}
+
+int ParseNumber(string stringNumber)
+{
+    return int.Parse(stringNumber);
+}
