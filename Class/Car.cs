4c529abd7b3cf1cd587c8fe6322fcbcc7d34b07a
Andrii Spivak <aspi@diceus.com>
2025-12-02 14:34:52 +0200

    Class

---
+++ Class/Car.cs
@@ -0,0 +1,65 @@
+﻿namespace Class
+{
+    internal class Car
+    {
+        public Car()
+        {
+            Count++;
+        }
+
+        public Car(int power, string model, int age)
+        {
+            Console.WriteLine("Hello. I am constructor");
+
+            _power = power;
+            _model = model;
+            Year = age;
+
+            Count++;
+        }
+
+        //Статична властивість
+        public static int Count { get; set; }
+
+        private int _power ;
+
+        public int GetPower()
+        {
+            return _power;
+        }
+
+        public void SetPower(int power)
+        {
+            _power = power;
+        }
+        
+        private string _model;
+
+        public string Model
+        {
+            get
+            {
+                return "My car is " + _model;
+            }
+
+            set
+            {
+                if (value.Contains("BMW"))
+                {
+                    _model = value + " M Series";
+                }
+                else
+                {
+                    _model = value;
+                }
+            }
+        }
+
+        public int Year { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"My car is {_model}. Power of my car is {_power} hp. Year of manufacture {Year}");
+        }
+    }
+}
---
+++ Class/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car(200, "Mercedes Sprinter", 20);
+
+            myCar.Print();
+
+            var carOfMyFather = new Car();
+
+            carOfMyFather.SetPower(70);
+            carOfMyFather.Model = "Moskvich";
+            carOfMyFather.Year = 50;
+
+            carOfMyFather.Print();
+
+            new Car();
+            new Car();
+            new Car();
+            new Car();
+            new Car();
+            new Car();
+            new Car();
+            new Car();
+
+            Console.WriteLine($"Count of Car class instances is {Car.Count}");
+
+            ////////////////////////////////////Building class////////////////////////////////////
+            
+            Console.WriteLine();
+
+            //Object initializer syntax
+            var building = new Building
+            {
+                StreetName = "Lennon",
+                Year = 1990
+            };
+
+            //Alternative way of setting properties
+            //building.StreetName = "Lennon";
+            //building.Year = 1990;
+
+            Console.WriteLine($"Building is on {building.StreetName} street and was built in {building.Year} year.");
+        }
+    }
+}
