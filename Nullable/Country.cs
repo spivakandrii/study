9dd5aa3eeed7aa6b9f8822995c8ee9cd5867b0ab
Andrii Spivak <aspi@diceus.com>
2025-12-15 09:16:33 +0200

    Nullable

---
+++ Nullable/Country.cs
@@ -0,0 +1,33 @@
+﻿namespace Nullable;
+
+public class Country
+{
+    public Country(string name, int  population, DateTime independenceDay, DateTime createdDate)
+    {
+        Name = name;
+        Population = population;
+        CreatedDate = createdDate;
+        IndependenceDay = independenceDay;
+    }
+
+    private string Name { get; }
+
+    private int Population { get; set; }
+
+    private DateTime IndependenceDay { get; }
+
+    private DateTime CreatedDate { get; }
+
+    private DateTime? UpdatedDate { get; set; }
+
+    public void Update(int population)
+    {
+        Population = population;
+        UpdatedDate =  DateTime.UtcNow;
+    }
+    
+    public void Print()
+    {
+        Console.WriteLine($"Name: {Name}, Population {Population}, IndependenceDay {IndependenceDay}, CreatedDate {CreatedDate}, UpdatedDate {UpdatedDate},");
+    }
+}
---
+++ Nullable/Program.cs
@@ -0,0 +1,31 @@
+﻿using Nullable;
+
+int? number = 1;
+
+Nullable<int> nullableNumber = 5;
+
+Console.WriteLine($"int? number {number.GetType()} is a primitive data type: {number.GetType().IsPrimitive}.");
+
+Console.WriteLine($"Nullable<int> {nullableNumber.GetType()} is a primitive data type: {nullableNumber.GetType().IsPrimitive}.");
+
+Console.WriteLine("int? {0} is a primitive data type: {1}.", typeof(int?).Name, typeof(int?).IsPrimitive);
+
+Console.WriteLine("int? {0} is a reference data type: {1}.", typeof(int?).Name, !(typeof(int?).IsValueType));
+
+var ukraine = new Country("Ukraine", 52000000, new DateTime(1991, 08, 24), DateTime.UtcNow);
+
+ukraine.Print();
+
+ukraine.Update(49000000);
+
+ukraine.Print();
+
+// Console.WriteLine(default(int));
+// Console.WriteLine(default(DateTime));
+// Console.WriteLine(default(decimal));
+// Console.WriteLine(decimal.MaxValue);
+// Console.WriteLine(double.MaxValue);
+// Console.WriteLine(float.MaxValue);
+// Console.WriteLine(int.MaxValue);
+// Console.WriteLine(long.MaxValue);
+// Console.WriteLine(DateTime.MaxValue);
